67dd95adb95ae949fc763765ef619571aace5115
MadhanG16 <mstark1608@gmail.com>
2026-03-19 12:14:43 +0530

    fix the error for creating SuperUser

--- z1fashion_modify/urls.py
+++ z1fashion_modify/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from products.views import ProductViewSet, create_superuser
+from products.views import ProductViewSet, create_admin
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
@@ -35,7 +35,7 @@ urlpatterns = [
     path('api/products/', ProductViewSet.as_view({'get': 'list'}), name='get_products'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('create-admin/', create_superuser, name='create_admin'),
+    path('create-admin/', create_admin, name='create_admin'),
 ]
 
 if settings.DEBUG:
